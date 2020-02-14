623262141b9fccccb4f38a3008290449dd895e47
Jake Pino <jakepino@yahoo.com>
2020-02-14 21:06:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,37 +5,37 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => "Black, White"
       :players => [{
-        :player_name => nil,
-        :number => nil,
-        :shoe => nil,
-        :points => nil
-        :rebounds => nil,
-        :assists => nil,
-        :steals => nil,
-        :blocks => nil,
-        :slam_dunks => nil
+        :player_name => "Alan Anderson",
+        :number => 0,
+        :shoe => 0,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
       }, 
       {
-        :player_name => nil,
-        :number => nil,
-        :shoe => nil,
-        :points => nil
-        :rebounds => nil,
-        :assists => nil,
-        :steals => nil,
-        :blocks => nil,
-        :slam_dunks => nil
+        :player_name => "Reggie Evans",
+        :number => 30,
+        :shoe => 14,
+        :points => 12,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 12,
+        :blocks => 12,
+        :slam_dunks => 7
       },
       {
-        :player_name => nil,
-        :number => nil,
-        :shoe => nil,
-        :points => nil
-        :rebounds => nil,
-        :assists => nil,
-        :steals => nil,
-        :blocks => nil,
-        :slam_dunks => nil
+        :player_name => "Brook Lopez",
+        :number => 11,
+        :shoe => 17,
+        :points => 17,
+        :rebounds => 19,
+        :assists => 10,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 15
       },
       {
         :player_name => nil,
@@ -59,8 +59,8 @@ def game_hash
         :blocks => nil,
         :slam_dunks => nil
       }]},
-    :away => {:team_name => "Brooklyn Nets",
-      :colors => "Black, White"
+    :away => {:team_name => "Charlotte Hornets",
+      :colors => "Turquoise, Purple"
       :players => [{
         :player_name => nil,
         :number => nil,
