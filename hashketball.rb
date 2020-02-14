91b4393529d308bf378b6d5cb5d58ed5575f7b72
Jake Pino <jakepino@yahoo.com>
2020-02-14 21:09:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -40,24 +40,24 @@ def game_hash
       {
         :player_name => "Mason Plumlee",
         :number => 1,
-        :shoe => nil,
-        :points => nil
-        :rebounds => nil,
-        :assists => nil,
-        :steals => nil,
-        :blocks => nil,
-        :slam_dunks => nil
+        :shoe => 19,
+        :points => 26,
+        :rebounds => 11,
+        :assists => 6,
+        :steals => 3,
+        :blocks => 8,
+        :slam_dunks => 5
       },
       {
-        :player_name => nil,
-        :number => nil,
-        :shoe => nil,
-        :points => nil
-        :rebounds => nil,
-        :assists => nil,
-        :steals => nil,
-        :blocks => nil,
-        :slam_dunks => nil
+        :player_name => "Jason Terry",
+        :number => 31,
+        :shoe => 15,
+        :points => 19,
+        :rebounds => 2,
+        :assists => 2,
+        :steals => 4,
+        :blocks => 11,
+        :slam_dunks => 1
       }]},
     :away => {:team_name => "Charlotte Hornets",
       :colors => "Turquoise, Purple"
