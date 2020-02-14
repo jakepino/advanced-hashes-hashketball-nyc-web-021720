0ed42ad334f20c2aee2b4093bc826b290ed8d1e2
Jake Pino <jakepino@yahoo.com>
2020-02-14 20:58:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,24 @@
 # Write your code here!
-
+def game_hash
+  game_info = {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => "Black, White"
+      :players => [{
+        :player_name => nil,
+        :number => nil,
+        :shoe => nil,
+        :points => nil
+        :rebounds => nil,
+        :assists => nil,
+        :steals => nil,
+        :blocks => nil,
+        :slam_dunks =>
+      }]
+      
+    }
+  }
+end
 
 
 
